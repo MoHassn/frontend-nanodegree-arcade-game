8b48102b50087df0b894a3576c67a9af93fb1d58
Mohamed Hassan <m.h.h.305405@gmail.com>
2019-11-16 17:40:43 +0200

    Update Enemy class with position and speed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+
+    // The position of the enemy
+    this.x = x;
+    this.y = y;
+    // The speed of the enemy
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
